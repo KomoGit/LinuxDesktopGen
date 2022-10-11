8d8691a4c73f37ebfddc17e6d1baa7f6c0920871
James <entreur@gmail.com>
2022-10-11 15:33:06 +0400

    Initial Commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+)
+
+/*
+Purpose of this tool:
+Try to create a .Desktop generator that will easily create executables for Linux Applications.
+The application should be controlled with a GUI so you will need to import fyne.
+UI will contain:
+# Dropdown - To select runner. Wine,native,etc.
+# Checkbox(es) - To whether run the app as terminal / Notification. ETC
+# User Input - For name,description,etc.
+# File Selection - For Icon.
+
+Some of these options can be optional.
+*/
+func main() {
+	log.Println("Hello World")
+}
